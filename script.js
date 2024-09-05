35892143345ab10e68b94ece858918a8b5438f04
Parth Shinde <Shindeparth003@gmail.com>
2024-09-05 23:22:49 +0530

    test on form

--- script.js
+++ script.js
@@ -64,3 +64,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function formalert() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => alert("Thank you for your submission"))
+      .catch((error) => alert(error));
+  };
+
+  document.querySelector("form").addEventListener("submit", handleSubmit);
+}
