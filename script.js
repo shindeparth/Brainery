be15f3216129baf36248c6286ad937ab15bc86f1
Parth Shinde <Shindeparth003@gmail.com>
2024-09-12 14:54:31 +0530

    optimizations

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-// Smooth scrolling with Locomotive Scroll (for index.html only)
-if (
-  window.location.pathname === "/index.html" ||
-  window.location.pathname === "/"
-) {
-  document.addEventListener("DOMContentLoaded", () => {
+// Smooth scrolling with Locomotive Scroll
+document.addEventListener("DOMContentLoaded", () => {
+  if (
+    window.location.pathname === "/index.html" ||
+    window.location.pathname === "/"
+  ) {
     const scroll = new LocomotiveScroll({
       el: document.querySelector(".smooth-scroll"),
       smooth: true,
-      lerp: 0.05, // Smoothness level for scrolling
+      lerp: 0.05,
     });
 
-    // Update Locomotive Scroll on window resize
-    window.addEventListener("resize", () => {
-      scroll.update();
-    });
-  });
-}
+    window.addEventListener("resize", () => scroll.update());
+  }
+});
 
 // Dialog handling
 document.addEventListener("DOMContentLoaded", () => {
   const openDialogBtns = document.querySelectorAll(".dialog-trigger");
   const closeDialogBtns = document.querySelectorAll(".dialog-close");
 
-  // Function to open a dialog
   function openDialog(index) {
     const dialogContainer =
       document.querySelectorAll(".dialog-container")[index];
@@ -39,7 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   }
 
-  // Function to close a dialog
   function closeDialog(dialogContainer) {
     gsap.to(dialogContainer, {
       opacity: 0,
@@ -54,25 +49,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Add event listeners for opening dialogs
-  openDialogBtns.forEach((btn, index) => {
-    btn.addEventListener("click", () => openDialog(index));
-  });
-
-  // Add event listeners for closing dialogs
-  closeDialogBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const dialogContainer = btn.closest(".dialog-container");
-      if (dialogContainer) {
-        closeDialog(dialogContainer);
-      }
-    });
-  });
-
-  // Close the dialog if clicked outside of it
+  openDialogBtns.forEach((btn, index) =>
+    btn.addEventListener("click", () => openDialog(index))
+  );
+  closeDialogBtns.forEach((btn) =>
+    btn.addEventListener("click", () =>
+      closeDialog(btn.closest(".dialog-container"))
+    )
+  );
   window.addEventListener("click", (event) => {
-    if (event.target.classList.contains("dialog-container")) {
+    if (event.target.classList.contains("dialog-container"))
       closeDialog(event.target);
-    }
   });
 });
